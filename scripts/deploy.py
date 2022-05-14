65c7a406c18122fe21524cf9170f7b5b94949c07
Sakib Rahman <srskb15@gmail.com>
2022-05-15 02:40:39 +0600

    SA | Ganache local network added

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,19 +1,18 @@
 from brownie import FundMe, MockV3Aggregator, accounts, network, config, network
-from scripts.helpers import get_account
-from web3 import Web3
+from scripts.helpers import deploy_mocks, get_account, LOCAL_BLOCKCHAIN_ENV
 
 
 def deploy():
     account = get_account()
+    print(account)
 
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENV:
         # if not in development mode, deploy to the account that is specified in the config file with associated network
-        price_feed = config["networks"][network.show_active()]["eth_usd_price_feed"]
+        price_feed = config["networks"][network.show_active()].get("eth_usd_price_feed")
         print(price_feed)
     else:
         # if in development mode, mocks the price feed
-        if len(MockV3Aggregator) > 0:
-            MockV3Aggregator.deploy(18, Web3.toWei(1, "ether"), {"from": account})
+        deploy_mocks()
         price_feed = MockV3Aggregator[-1].address
 
     contract = FundMe.deploy(
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,9 +1,21 @@
-from brownie import accounts, network, config
+from brownie import accounts, network, config, MockV3Aggregator, accounts
+from web3 import Web3
+
+DECIMALS = 18
+STARTING_PRICE = 2000
+LOCAL_BLOCKCHAIN_ENV = ["development", "ganache-local"]
 
 
 def get_account():
     print(network.show_active())
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENV:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
+        )
