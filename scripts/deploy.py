a0418f2fd9da564ecc667b9df1c46dca7cdd8b33
Sakib Rahman <srskb15@gmail.com>
2022-05-15 02:14:07 +0600

    SA | code added for price_feed

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,26 @@
-from brownie import FundMe, accounts, network
+from brownie import FundMe, MockV3Aggregator, accounts, network, config, network
 from scripts.helpers import get_account
+from web3 import Web3
 
 
 def deploy():
     account = get_account()
-    contract = FundMe.deploy({"from": account}, publish_source=True)
+
+    if network.show_active() != "development":
+        # if not in development mode, deploy to the account that is specified in the config file with associated network
+        price_feed = config["networks"][network.show_active()]["eth_usd_price_feed"]
+        print(price_feed)
+    else:
+        # if in development mode, mocks the price feed
+        if len(MockV3Aggregator) > 0:
+            MockV3Aggregator.deploy(18, Web3.toWei(1, "ether"), {"from": account})
+        price_feed = MockV3Aggregator[-1].address
+
+    contract = FundMe.deploy(
+        price_feed,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(contract.address)
 
 
