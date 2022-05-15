f9da621d1829c880757b6b96c7e7aa68d2f2946b
Sakib Rahman <srskb15@gmail.com>
2022-05-15 10:48:54 +0600

    SA | deploy.py final version

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,12 +4,12 @@ from scripts.helpers import deploy_mocks, get_account, LOCAL_BLOCKCHAIN_ENV
 
 def deploy():
     account = get_account()
-    print(account)
+    print("Account: {}".format(account))
 
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENV:
         # if not in development mode, deploy to the account that is specified in the config file with associated network
         price_feed = config["networks"][network.show_active()].get("eth_usd_price_feed")
-        print(price_feed)
+        print("Price Feed: {}".format(price_feed))
     else:
         # if in development mode, mocks the price feed
         deploy_mocks()
