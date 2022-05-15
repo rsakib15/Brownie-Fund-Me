e144f7b9aa18ecdc031bfea6e45333309bc86660
Sakib Rahman <srskb15@gmail.com>
2022-05-15 12:08:14 +0600

    SA | Funding intial version

---
+++ scripts/fund_withdraw.py
@@ -0,0 +1,20 @@
+from brownie import FundMe
+from scripts.helpers import get_account
+
+
+def fund():
+    print("Funding")
+    fund_me = FundMe[-1]
+    entrance_fee = fund_me.getEntranceFee()
+    print("Price: {}".format(fund_me.getPrice()))
+    print("Entrance Fee: {}".format(fund_me.getEntranceFee()))
+    fund_me.fund({"from": get_account(), "value": entrance_fee})
+
+
+def withdraw():
+    print("Withdrawing")
+
+
+def main():
+    fund()
+    # withdraw()
